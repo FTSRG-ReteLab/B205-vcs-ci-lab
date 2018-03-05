159d9d6b65648b6260118b229e94e7f8b20c8263
TarapcsakBendeguz <tarap.beni@gmail.com>
2018-03-05 06:23:19 -0800

    1. feladat Beni

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -2,11 +2,14 @@ package hu.bme.mit.train.user;
 
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private Timer timer = new Timer();
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
@@ -26,6 +29,17 @@ public class TrainUserImpl implements TrainUser {
 	public void overrideJoystickPosition(int joystickPosition) {
 		this.joystickPosition = joystickPosition;
 		controller.setJoystickPosition(joystickPosition);
+	}
+	
+	@Override
+	public void joystickPositionChanged(int joystickPosition){
+		timer.scheduleAtFixedRate(new TimerTask() {
+			  @Override
+			  public void run() {
+				  controller.setJoystickPosition(joystickPosition);
+				  controller.followSpeed();
+			  }
+			  },0,60*1000);
 	}
 	
 	public int doNot(int x){
